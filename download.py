2089cca6dd1376401857797765a959e6ea065527
Ivan Gorskiy <gorskii241152003@gmail.com>
2026-02-07 11:28:15 +0300

    asf

---
+++ download.py
@@ -0,0 +1,4 @@
+from modelscope import snapshot_download
+snapshot_download('iic/CosyVoice-300M', local_dir='pretrained_models/CosyVoice-300M')
+
+snapshot_download('FunAudioLLM/Fun-CosyVoice3-0.5B-2512', local_dir='pretrained_models/Fun-CosyVoice3-0.5B')
